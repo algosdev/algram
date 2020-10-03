a350dbf7f0ec7c0e6be0b6bd34b932af68fa8b07
Algo <68058088+algosdev@users.noreply.github.com>
2020-10-03 22:59:40 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import SignIn from "./components/SignIn";
-// import SignUp from "./components/SignUp";
+import SignUp from "./components/SignUp";
 import ChatRoom from "./components/ChatRoom";
 function App() {
   return (
