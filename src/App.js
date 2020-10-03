80284655b6b75b47880678c45b68c1aac395272e
Algo <68058088+algosdev@users.noreply.github.com>
2020-10-03 22:56:40 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path="/" component={SignIn} />
-        {/* <Route exact path="/signup" component={SignUp} /> */}
+        <Route exact path="/signup" component={SignUp} />
         <Route exact path="/chat" component={ChatRoom} />
       </div>
     </Router>
